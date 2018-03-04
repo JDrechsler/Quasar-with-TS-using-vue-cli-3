b344e5b85fd900969031678a6c7198ac3f63aeaf
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2018-03-04 18:32:30 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,6 @@
 
 <script lang=ts>
 import { Component, Vue } from 'vue-property-decorator';
-import { truncate } from 'fs';
 
 @Component({
 
@@ -84,9 +83,5 @@ export default class App extends Vue {
 	loremipsum: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }
 
-
-</script>
-'
-}
 
 </script>
